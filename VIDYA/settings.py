a6618bff4163bb4f21d8d8c1474d96528c16fd8d
Gupta2001 <80310618+Gupta2001@users.noreply.github.com>
2022-07-28 20:30:52 +0530

    Update settings.py

--- VIDYA/settings.py
+++ VIDYA/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
